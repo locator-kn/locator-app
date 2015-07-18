16ede16d83d73e2fe542bdfd400a9b78b72bc300
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-07-18 16:57:02 +0200

    refactor project site

--- app/js/main.js
+++ app/js/main.js
@@ -149,72 +149,7 @@ $(document).ready(function () {
     var errorBorder = {border: "1.5px solid #FF0000"};
     var normalBorder = {border: "1px solid #e6e6e6"};
 
-    $(".registration-submit").click(function (event) {
-        event.preventDefault();
 
-        var $form = $("#registration");
-        var name = $form.find('input[name="name"]');
-        var mail = $form.find('input[name="email"]');
-
-        name.css(normalBorder);
-        mail.css(normalBorder);
-
-
-        if (!name.val()) {
-            name.css(errorBorder);
-            return;
-        } else if (!mail.val() || !(/[^\s@]+@[^\s@]+\.[^\s@]+/).test(mail.val())) {
-            mail.css(errorBorder);
-            return;
-        }
-
-        var register = {
-            name: name.val(),
-            mail: mail.val()
-        };
-
-
-        $.ajax({
-            type: 'POST',
-            url: '/mail/register',
-            contentType: 'application/json',
-            data: JSON.stringify(register),
-            success: registrationSuccess,
-            error: registrationError,
-            processData: false
-        });
-    });
-
-    function registrationSuccess() {
-        $("#registration").fadeOut(function () {
-            $(".success").fadeIn("thanks");
-        });
-
-    }
-
-    function registrationError(resp) {
-        var err = (resp.status === 409 ? 'conflict' : 'generic');
-
-        $("#registration").fadeOut(function () {
-            $(".error").addClass(err);
-        });
-
-        setTimeout(function () {
-            $(".error").removeClass(err).fadeOut(function () {
-                $("#registration").fadeIn();
-            });
-
-        }, 2000)
-
-    }
-
-// pseudo registration
-    $(".registration-submit-bottom").click(function (e) {
-        $('html, body').stop().animate({
-            scrollTop: 0
-        }, 800);
-        e.preventDefault();
-    });
 
 
 // feedback
